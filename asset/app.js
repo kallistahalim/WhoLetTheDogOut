80f0eb678343c1ce346ce7a818e733c53ec2efe6
kallistahalim <kallistahalim@gmail.com>
2020-04-15 17:44:25 -0700

    it worked roughly

--- asset/app.js
+++ asset/app.js
@@ -1,9 +1,17 @@
 //set up variable of people who can take the dog out
     //Nenad - Kallista
 
+var hooman = ["Kallista", "Nenad"];
+
 
 //set up randomizer to determine(by computer) who will walk the dog
     //Math.floor(Math.random) * number of indexes in variable above
         //if that person is picked, the other person has to do dishes and clean the kitchen
+
+document.onkeypress = function() {
+    var computerDecision = hooman[Math.floor(Math.random()* hooman.length)];
+    console.log(computerDecision);
+    document.querySelector("#button").innerHTML = computerDecision;
+}
 
 //show it on the browser
