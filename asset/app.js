3413a040616a5172156ff58b2921f9e4a0b7bcf5
kallistahalim <kallistahalim@gmail.com>
2020-04-15 17:16:02 -0700

    pseudocode the js

---
+++ asset/app.js
@@ -0,0 +1,9 @@
+//set up variable of people who can take the dog out
+    //Nenad - Kallista
+
+
+//set up randomizer to determine(by computer) who will walk the dog
+    //Math.floor(Math.random) * number of indexes in variable above
+        //if that person is picked, the other person has to do dishes and clean the kitchen
+
+//show it on the browser
